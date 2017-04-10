59d56e3ccb20925cdfb1930105685f8a17201b6f
Valdis Līcis <valdis.licis@gmail.com>
2017-04-10 14:44:06 +0100

    Initial commit

---
+++ example.php
@@ -0,0 +1,16 @@
+<?php
+
+// Start sessions - we are going to need them to save API calls
+session_start();
+
+// Load all required dependencies
+require_once 'vendor/autoload.php';
+
+// Use the appropriate class
+use Instasass\Instasassapi;
+
+// Initialize class and connect to the service
+$oInstaSass = new Instasassapi(API_KEY);
+
+// Do a test - feeding in a SASS code
+var_dump($oInstaSass->sass2cssSaved(__DIR__ . '/style.scss'));
